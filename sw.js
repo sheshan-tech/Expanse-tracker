a78206759693dbf48a3ae7cc0909229b5f0aa8e2
sheshan-tech <sheshansingh4u@gmail.com>
2026-09-09 17:25:06 +0530

    V4

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='expense-tracker-test-v3';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='expense-tracker-v4';const ASSETS=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==='GET'){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));}return r}).catch(()=>caches.match(e.request))));
